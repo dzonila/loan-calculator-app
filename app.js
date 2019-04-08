b1ce6d2c34cc96f813012309ee5dd0856301edd5
Nikola Stamenkovic <nikolastameni88@gmail.com>
2019-04-08 17:18:46 +0200

    loader ui improved and working

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Listen for submit on form
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log('Calculating...');
   // UI Vars grab
   const amount = document.getElementById('amount');
@@ -24,15 +33,23 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly*calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    
+    // show results
+    document.getElementById('results').style.display = 'block';
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
   // create a div
   const errorDiv = document.createElement('div');
 
